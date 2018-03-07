da2442cdd8a4de14c0fad9ab3782f88a0f968b84
shawn <liushawn@outlook.com>
2018-03-07 19:06:40 +0800

    LinkedList

---
+++ src/collection/MyStack.java
@@ -0,0 +1,33 @@
+package collection;
+
+import java.util.LinkedList;
+
+import charactor.Hero;
+
+public class MyStack implements Stack{
+	
+	LinkedList <Hero> heros = new LinkedList<>();
+	public void push(Hero h){
+		heros.addLast(h);
+	}
+	public Hero pull(){
+		return heros.removeLast();
+	}
+	public Hero peek(){
+		return heros.getLast();
+	}
+	
+	public static void main(String[] args) {
+		MyStack heroStack = new MyStack();
+		for(int i=0;i<5;i++){
+			Hero h = new Hero("hero name" + i);
+			System.out.println("压入hero:" + h);
+			heroStack.push(h);
+		}
+		for(int i=0;i<5;i++){
+			Hero h = heroStack.pull();
+			System.out.println("弹出hero：" + h);
+		}
+	}
+
+}
---
+++ src/collection/Stack.java
@@ -0,0 +1,13 @@
+package collection;
+
+import charactor.Hero;;
+
+public interface Stack {
+	
+
+	public void push(Hero h);
+	
+	public Hero pull();
+	
+	public Hero peek();
+}
