17cd170efe8ec341a689494119a731b9db5c9281
shawn <liushawn@outlook.com>
2018-03-08 14:01:47 +0800

    常见线程方法

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,30 +1,63 @@
+//package charactor;
+//
+//import java.io.Serializable;
+// 
+//public class Hero{
+//    public String name; 
+//    public float hp;
+//    
+//    public int damage;
+//    
+//    public void attackHero(Hero h) {
+//    	try {
+//    		//为了表示攻击需要时间，每次攻击暂停1000毫秒
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//    	h.hp-=damage;
+//    	System.out.format("%s 正在攻击 %s, %s的血变成了 %.0f%n",name,h.name,h.name,h.hp);
+//    	
+//    	if(h.isDead())
+//    		System.out.println(h.name +"死了！");
+//    }
+//
+//	public boolean isDead() {
+//		return 0>=hp?true:false;
+//	}
+//
+//}
 package charactor;
-
-import java.io.Serializable;
  
+import java.io.Serializable;
+  
 public class Hero{
     public String name; 
     public float hp;
-    
+     
     public int damage;
-    
+     
     public void attackHero(Hero h) {
-    	try {
-    		//为了表示攻击需要时间，每次攻击暂停1000毫秒
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	h.hp-=damage;
-    	System.out.format("%s 正在攻击 %s, %s的血变成了 %.0f%n",name,h.name,h.name,h.hp);
-    	
-    	if(h.isDead())
-    		System.out.println(h.name +"死了！");
+    	//把暂停时间去掉，多条线程各自会尽力去占有CPU资源
+    	//线程的优先级效果才可以看得出来
+//        try {
+//            
+//            Thread.sleep(0);
+//        } catch (InterruptedException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+        h.hp-=damage;
+        System.out.format("%s 正在攻击 %s, %s的血变成了 %.0f%n",name,h.name,h.name,h.hp);
+         
+        if(h.isDead())
+            System.out.println(h.name +"死了！");
     }
-
-	public boolean isDead() {
-		return 0>=hp?true:false;
-	}
-
+ 
+    public boolean isDead() {
+        return 0>=hp?true:false;
+    }
+ 
 }
+
