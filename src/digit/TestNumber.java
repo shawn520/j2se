507bc910b8aabf09ada190594a23d04d9f11f965
shawn <liushawn@outlook.com>
2018-05-14 20:56:17 +0800

    自动装箱和自动拆箱

--- src/digit/TestNumber.java
+++ src/digit/TestNumber.java
@@ -6,9 +6,25 @@ public class TestNumber {
 		// TODO Auto-generated method stub
 		
 		int i1 = 5;
+		
+		//基本类型转换成封装类型
 		Integer it = new Integer(i1);
+		
+		//自动转换叫装箱
+		Integer it2 = i1;
+		
+		System.out.println(it2.intValue());
+		
+		//封装类型转换成基本类型
 		int i2 = it.intValue();
 		System.out.println(i2);
+		
+		//自动转换叫拆箱
+		int i3 = it;
+		System.out.println("i3="+i3);
+		
+		
+		System.out.println(it instanceof Number);
 
 	}
 
