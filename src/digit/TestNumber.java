3b97ff8e3e212c0c3f9d17a5d2e14289c631899d
shawn <liushawn@outlook.com>
2018-05-14 20:48:07 +0800

    字符串，封装类

---
+++ src/digit/TestNumber.java
@@ -0,0 +1,15 @@
+package digit;
+
+public class TestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i1 = 5;
+		Integer it = new Integer(i1);
+		int i2 = it.intValue();
+		System.out.println(i2);
+
+	}
+
+}
