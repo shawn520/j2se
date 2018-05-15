e923ce9bff2e3299c92cb3e86a600f89be517612
shawn <liushawn@outlook.com>
2018-05-15 11:35:14 +0800

    总长度，左对齐，补0，千位分隔符，小数点位数，本地化表达

--- src/digit/TestNumber4.java
+++ src/digit/TestNumber4.java
@@ -18,6 +18,28 @@ public class TestNumber4 {
 		
 		System.out.printf("这是换行符%n");
 		System.out.printf("这是换行符%n");
+		
+		//总长度，左对齐，补0，千位分隔符，小数点位数，本地化表达
+		int year = 2020;
+		
+		//直接打印数字
+		System.out.format("%d%n", year);
+		
+		//总长度8，默认右对齐
+		System.out.format("%8d%n", year);
+		
+		//总长度8，左对齐
+		System.out.format("%-8d%n", year);
+		
+		//总长度8，不够补0
+		System.out.format("%08d%n", year);
+		
+		//千位分割符
+		System.out.format("%,08d%n", year*1000000);
+		
+		//小数点位数
+		System.out.format("%.2f%n", Math.PI);
+		
 	}
 
 }
