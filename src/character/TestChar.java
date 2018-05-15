0223f65b161beddd671c8447fb530c68534fa740
shawn <liushawn@outlook.com>
2018-05-15 13:56:17 +0800

    char自动装箱和拆箱

---
+++ src/character/TestChar.java
@@ -0,0 +1,23 @@
+package character;
+
+public class TestChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char c1 = 'a';
+		
+		//自动装箱
+		Character c = c1;
+		
+		//自动拆箱
+		c1 = c;
+		
+		char c2 = '1';
+		char c3 = '中';
+		char c4 = 'b';
+		
+		
+	}
+
+}
