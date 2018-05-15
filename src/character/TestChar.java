d9d217b5e8312a9d50e19338dac2652955e6caa5
shawn <liushawn@outlook.com>
2018-05-15 16:31:39 +0800

    character的常用方法

--- src/character/TestChar.java
+++ src/character/TestChar.java
@@ -17,7 +17,17 @@ public class TestChar {
 		char c3 = '中';
 		char c4 = 'b';
 		
+		//判断是否为字母
+		System.out.println(Character.isLetter('a'));
 		
+		//判断是否为数字
+		System.out.println(Character.isDigit('5'));
+		
+		//判断是否为空格
+		System.out.println(Character.isWhitespace(' '));
+		
+		//判断是否为小写字母
+		System.out.println(Character.isLowerCase('A'));
 	}
 
 }
