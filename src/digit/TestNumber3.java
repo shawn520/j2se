a272743db1a9184c71a32cc2103172e5798fc0f8
shawn <liushawn@outlook.com>
2018-05-15 10:14:29 +0800

    四舍五入

---
+++ src/digit/TestNumber3.java
@@ -0,0 +1,16 @@
+package digit;
+
+public class TestNumber3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float f1 = 5.4f;
+		float f2 = 5.5f;
+		//四舍
+		System.out.println(Math.round(f1));
+		//五入
+		System.out.println(Math.round(f2));
+	}
+
+}
