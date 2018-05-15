14e82cecdd56e706cb53de68af9e90ea68630ab0
shawn <liushawn@outlook.com>
2018-05-15 10:22:31 +0800

    生成随机数

--- src/digit/TestNumber3.java
+++ src/digit/TestNumber3.java
@@ -11,6 +11,15 @@ public class TestNumber3 {
 		System.out.println(Math.round(f1));
 		//五入
 		System.out.println(Math.round(f2));
+		
+		//得到一个0-1之间的随机浮点数
+		System.out.println(Math.random());
+		
+		//得到一个0-10之间的随机浮点数
+		System.out.println(Math.random()*10);
+		
+		//生成0-10之间的随机整数
+		System.out.println((int)(Math.random()*10));
 	}
 
 }
