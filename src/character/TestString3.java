150aba1b79f675f51a2291f51b984c61390be0f0
shawn <liushawn@outlook.com>
2018-05-16 17:12:44 +0800

    StringBuffer的性能

--- src/character/TestString3.java
+++ src/character/TestString3.java
@@ -25,5 +25,34 @@ public class TestString3 {
 		StringBuffer str2 = new StringBuffer(str1);
 		System.out.println(str2.length());
 		System.out.println(str2.capacity());
+		
+		//练习
+		testString();
+		testStringBuffer();
+		
+	}
+	
+	public static void testString(){
+		String str3 = "su";
+		String str4 = "zhou";
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 10000; i++) {
+			str3 = str3 + str4;
+		}
+		long end = System.currentTimeMillis();
+		long time = end - start;
+		System.out.println("String Time"+time);
+	}
+	public static void testStringBuffer(){
+		
+		StringBuffer sb = new StringBuffer("su");
+		String str4 = "zhou";
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 10000; i++) {
+			sb.append(str4);
+		}
+		long end = System.currentTimeMillis();
+		long time = end - start;
+		System.out.println("StringBuffer Time"+time);
 	}
 }
