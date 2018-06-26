7bc4111fc4ec68c15e500c8bbcf858318a03a228
shawn <liushawn@outlook.com>
2018-06-26 19:11:24 +0800

    注意数组边界

--- src/array/BubbleSort.java
+++ src/array/BubbleSort.java
@@ -2,10 +2,15 @@ package array;
 
 public class BubbleSort {
 	//冒泡排序
-	public static int [] sort(int [] a){
+	public static void sort(int [] a){
+		
+		//增加判断
+		if(a==null || a.length == 0 || a.length == 1){
+			return ;
+		}
 		
 		for (int i = 0; i < a.length - 1; i++) {
-			for (int j = 0; j < a.length - 1; j++) {
+			for (int j = 0; j < a.length - 1; j++) { //注意数组边界
 				if(a[j] > a[j+1] ){
 					int temp = a[j];
 					a[j] = a[j+1];
@@ -14,7 +19,9 @@ public class BubbleSort {
 			}
 		}
 		
-		return a;
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+"\t");
+		}
 	}
 
 }
