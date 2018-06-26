a2daaa3df85e77a83c261d505b0386bcede93dc3
shawn <liushawn@outlook.com>
2018-06-26 19:21:12 +0800

    冒泡排序，优化外层循环

--- src/array/BubbleSort.java
+++ src/array/BubbleSort.java
@@ -10,12 +10,19 @@ public class BubbleSort {
 		}
 		
 		for (int i = 0; i < a.length - 1; i++) {
+			
+			boolean flag = false;
 			for (int j = 0; j < a.length - 1; j++) { //注意数组边界
+				//冒泡排序优化：如果一趟排序下来，没有发生交换，则数组有序。
 				if(a[j] > a[j+1] ){
 					int temp = a[j];
 					a[j] = a[j+1];
 					a[j+1] = temp;
+					flag = true;
 				}
+			}
+			if(flag == false){
+				break;
 			}
 		}
 		
