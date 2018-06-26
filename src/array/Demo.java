42f3073ad39281a9973ff2bf14d04c7d4f3ad2b1
shawn <liushawn@outlook.com>
2018-06-26 16:37:58 +0800

    选择排序

---
+++ src/array/Demo.java
@@ -0,0 +1,27 @@
+package array;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] array = new int [5];
+		
+		//赋值
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int)(Math.random()*100);	
+		}
+		
+		//输出排序前的数组
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]+"\t");
+		}
+		System.out.println("\n" + "排序后的数组为");
+		array = SelectSort.selectSort(array);
+		//输出排序前的数组
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]+"\t");
+		}
+		
+	}
+
+}
---
+++ src/array/SelectSort.java
@@ -0,0 +1,21 @@
+package array;
+
+public class SelectSort {
+
+	public static int [] selectSort(int [] a){
+		int length = a.length;
+		for (int i = 0; i < a.length; i++) {
+			
+			for (int j = i+1; j < a.length; j++) {
+				if(a[j] < a[i]){
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+			
+		}
+		
+		return a;
+	}
+}
